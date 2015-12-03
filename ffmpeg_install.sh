f172e4ac188673274389a06bd3f2eae46f3410cc
Giancarlo Grasso <giancarlo@Giancarlos-MacBook-Pro.local>
2015-12-03 15:41:47 +0000

    First commit

---
+++ ffmpeg_install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+dir=${1-/local/$USER/software}
+
+mkdir -p $dir
+cd /tmp
+
+ffmpegurl=https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+
+ffmpeg_dl=${ffmpegurl##h*/}
+ffmpeg_dir=ffmpeg
+
+wget $ffmpegurl
+
+tar jxvf ffmpeg-snapshot.tar.bz2
+
+cd $ffmpeg_dir
+
+./configure --prefix=$dir
+make
+make install
---
+++ python_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+dir=${1-/local/$USER/usr}
+
+python_url=https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
+getpip_url=https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+
+python_dl=${python_url##h*/}
+python_dl=${python_dl%.tar.xz}
+
+mkdir -p $dir
+cd $dir
+
+wget $python_url
+wget $getpip_url
+
+tar xf "$python_dl".tar.xz
+
+cd $python_dl
+
+./configure --prefix=$dir
+make
+make install
+
+cd $dir
+
+ln -s $dir/bin/python2.7 $dir/bin/python
+export PATH=$dir/bin:$PATH
+
+python get-pip.py
+
+pip install numpy
+pip install scipy matplotlib sympy virtualenv virtualenvwrapper
