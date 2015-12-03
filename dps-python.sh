3df148ed5ff03d1602fd77c829782811b140d5b7
Giancarlo Grasso <giancarlo@Giancarlos-MacBook-Pro.local>
2015-12-03 16:38:41 +0000

    add source file and change directory name

--- dps-python.sh
+++ dps-python.sh
@@ -1,6 +1,6 @@
 # User specific aliases and functions
 dir=python
-export LD_LIBRARY_PATH=/local/$USER/$dir/Python-2.7.10:/local/$USER/$dir/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/local/$USER/$dir/lib:$LD_LIBRARY_PATH
 export PATH=/local/$USER/python/bin:$PATH
 export WORKON_HOME=/local/$USER/$dir/env
 source /local/$USER/$dir/bin/virtualenvwrapper.sh
