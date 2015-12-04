876cb210a340f0424371818104c4e4c01772cc19
Giancarlo Grasso <giancarlo@Giancarlos-MacBook-Pro.local>
2015-12-04 23:24:45 +0000

    add check for virtualenv

--- dps-python.sh
+++ dps-python.sh
@@ -1,10 +1,15 @@
 # User specific aliases and functions
 dir=python
+if [ ! -a /local/$USER/$dir/bin/python ]; then
+    echo "Python has not been installed on this machine"
+fi
 export LD_LIBRARY_PATH=/local/$USER/$dir/lib:$LD_LIBRARY_PATH
 export PATH=/local/$USER/python/bin:$PATH
 export WORKON_HOME=/local/$USER/$dir/env
 if [ -f /local/$USER/$dir/bin/virtualenvwrapper.sh ]; then
     source /local/$USER/$dir/bin/virtualenvwrapper.sh
+else
+    echo "virtualenv is not installed"
 fi
 
 if [ "${HOSTNAME##dps}" -gt "99" ]; then
